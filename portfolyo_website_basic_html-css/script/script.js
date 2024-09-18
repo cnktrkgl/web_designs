3cf0bfc7fe6c5e5de1e0397a85805d06c38b3e87
Cenk Türkoğlu <122122850+cnktrkgl@users.noreply.github.com>
2024-09-18 12:23:35 +0300

    Add files via upload

--- portfolyo_website_basic_html-css/script/script.js
+++ portfolyo_website_basic_html-css/script/script.js
@@ -1,15 +1,30 @@
 const hamburger = document.getElementById("hamburger");
 const nav_list = document.getElementById("nav_list_responsive");
+const links = Array.from(
+  document.getElementsByClassName("nav_list_responsive_link")
+);
+const darkmode_btn = document.getElementById("dark_mode_btn");
+const darkmode_btn_mobile = document.getElementById("mobile_dark_mode_btn");
+const body = document.body;
+
 hamburger.addEventListener("click", () => {
   nav_list.classList.toggle("active");
 });
 
-// const navBtn = document.querySelector(".navBtn");
-// const navbar = document.querySelector(".navbar");
-// const navClose = document.querySelector(".navClose");
-// navBtn.addEventListener("click", () => {
-//   navbar.classList.add("showNavbar");
-// });
-// navClose.addEventListener("click", () => {
-//   navbar.classList.remove("showNavbar");
-// });
+links.forEach((element) => {
+  element.addEventListener("click", () => {
+    nav_list.classList.remove("active");
+  });
+});
+
+darkmode_btn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
+});
+
+darkmode_btn_mobile.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
+});
+
+body.classList.add("light-mode");
